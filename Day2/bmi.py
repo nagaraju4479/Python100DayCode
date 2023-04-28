e67c2d2cd1f9846bdcb3235ffe1f23ccb8944648
nagaraju4479 <yajjuvarapu.nagaraju@gmail.com>
2023-04-28 22:07:41 +0530

    Day2 added BMI program

---
+++ Day2/bmi.py
@@ -0,0 +1,17 @@
+weight = float(input("Enter your weight in Kgs : "))
+height = float(input("Enter your height in meters : "))
+
+#BMI calculation
+
+bmi = weight / (height * height)
+print("******************************\n")
+
+print("Your BMI value is : "+str(bmi))
+if bmi<= 18.4:
+    print("You are under weight , please eat more Fat items !")
+elif bmi>= 18.5 and bmi<=24.9:
+    print("You are Normal weight - Congratulations!")
+elif bmi >=25 and bmi <= 39.9:
+    print("You are over weight - do proper deit and Exercises regulerly!")
+else:
+    print("You are Obesity , please contact your nutrition and reduce weight")
