7afce4ecc3f2950efbf226898803a504e91819ac
nagaraju4479 <yajjuvarapu.nagaraju@gmail.com>
2023-04-28 21:29:28 +0530

    Day 2 commit 1

---
+++ Day2/add.py
@@ -0,0 +1,10 @@
+number = input("Enter any two digit number : \n")
+number_str = str(number)
+print("************\n")
+
+number1 = int(number_str[0])
+number2 = int(number_str[1])
+
+sum = number1 + number2
+
+print(sum)
