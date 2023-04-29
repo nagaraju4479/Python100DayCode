86ecc4ae6cfb0710deb7c8fcaa14a8dff57db2b0
nagaraju4479 <yajjuvarapu.nagaraju@gmail.com>
2023-04-29 10:54:46 +0530

    modified tipClaculator program

--- Day2/tipCalculator.py
+++ Day2/tipCalculator.py
@@ -1,12 +1,13 @@
-bill_amount = float(input("Enter your bill amount : "))
+bill_amount = float(input("Enter your bill amount :$"))
 tip_in_percentage = int(input("What percentage you want give as a Tip --> 10 , 12, 15 ,20 ...? "))
 people_to_share = int(input("Enter number of people to share the amount : "))
 
 #Calculation
 tip_amount = (tip_in_percentage/bill_amount)* 100
-total_bill = bill_amount + tip_amount
-individual_share = total_bill / people_to_share
+total_bill = round((bill_amount + tip_amount),2)
+individual_share = round((total_bill / people_to_share),2)
 
 print("*************************\n")
-print(f"Total bill amount : {round(total_bill,2)}")
-print(f"Share per person : {round(individual_share,2)}")
+print(f"Total bill amount : ${total_bill}")
+print(f"Share per person : ${individual_share}")
+0
